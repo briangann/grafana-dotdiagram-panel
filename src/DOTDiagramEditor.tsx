76482f57f395759fef8882c6b315e42b0abe7995
Brian Gann <bkgann@gmail.com>
2019-12-24 18:55:46 -0600

    add editor options and colorpicker

--- src/DOTDiagramEditor.tsx
+++ src/DOTDiagramEditor.tsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
-import { Select } from '@grafana/ui';
+import { ColorPicker } from '@grafana/ui';
+import { Select, Tooltip } from '@grafana/ui';
 import { PanelEditorProps } from '@grafana/data';
 import { SelectableValue } from '@grafana/data';
 
@@ -15,13 +16,51 @@ export class DOTDiagramEditor extends PureComponent<PanelEditorProps<DOTDiagramO
   onFontSizeChanged = (value: SelectableValue<string>) => {
     this.props.onOptionsChange({ ...this.props.options, fontSize: value });
   };
+  onChangeLabelColor = (value: string) => {
+    this.props.onOptionsChange({ ...this.props.options, labelColor: value });
+  };
 
   render() {
     const { options } = this.props;
     return (
-      <div className="section gf-form-group">
-        <h5 className="section-heading">Display</h5>
-        <Select width={8} options={fontSizes} onChange={this.onFontSizeChanged} value={options.fontSize} />
+      <div>
+        <div className="section gf-form-group">
+          <h5 className="section-heading">Display Settings</h5>
+
+          <div className="gf-form">
+            <label className="gf-form-label width-11">Edge Color
+                <Tooltip content={"Edge Color TT"}>
+                <i className="fa fa-info-circle" />
+              </Tooltip>
+            </label>
+          </div>
+          <div className="gf-form">
+            <label className="gf-form-label width-11">Font Size
+                <Tooltip content={"Font Size TT"}>
+                <i className="fa fa-info-circle" />
+              </Tooltip>
+            </label>
+            <Select width={8} options={fontSizes} onChange={this.onFontSizeChanged} value={options.fontSize} />
+          </div>
+          <div className="gf-form">
+            <label className="gf-form-label width-11">Label Color
+              <Tooltip content={"Label Color TT"}>
+                <i className="fa fa-info-circle" />
+              </Tooltip>
+            </label>
+            <div className="labelcolor-input-color">
+              {options.labelColor && (
+                <div className="thresholds-row-input-inner-color-colorpicker">
+                  <ColorPicker
+                    color={options.labelColor}
+                    onChange={this.onChangeLabelColor}
+                    enableNamedColors={true}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/editorOptions.ts
+++ src/editorOptions.ts
@@ -1,13 +1,22 @@
 import { SelectableValue } from '@grafana/data';
-import { fontSizes } from './types';
+
+import { fontSizes, edgeColorOptions, nodeAlignmentOptions, operatorOptions } from './types';
 
 export interface DOTDiagramOptions {
+  edgeColor: SelectableValue<string>;
   fontSize: SelectableValue<string>;
+  labelColor: string;
+  nodeAlignment: SelectableValue<string>;
+  operator: SelectableValue<string>;
   unitFormat: SelectableValue<string>;
 }
 
 export const DOTDiagramDefaults: DOTDiagramOptions = {
+  edgeColor: edgeColorOptions[0],
   fontSize: fontSizes[0],
+  labelColor: "white",
+  nodeAlignment: nodeAlignmentOptions[0],
+  operator: operatorOptions[0],
   unitFormat: {
     "text": "short",
     "value": "short"
