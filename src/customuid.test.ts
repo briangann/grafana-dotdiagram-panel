fe05924d90928f3be81a61ac75ac1328218864ff
Brian Gann <bkgann@gmail.com>
2019-12-27 19:12:47 -0600

    update test

--- src/customuid.test.ts
+++ src/customuid.test.ts
@@ -1,7 +1,15 @@
+import { CustomUID } from "./customuid";
 describe('customuuid test', () => {
-  const text = 'hello';
+  const text = 'random_urlpath';
+  let uid = new CustomUID(text);
 
-  it('should work', () => {
-    expect(text).toBe('hello');
+  it('should generate an href', () => {
+    expect(uid.href).toBe("http://localhost/#O-random_urlpath-1");
+  });
+  it('should generate an id', () => {
+    expect(uid.id).toBe("O-random_urlpath-1");
+  });
+  it('should generate an url', () => {
+    expect(uid.toString()).toBe("url(http://localhost/#O-random_urlpath-1)");
   });
 });
