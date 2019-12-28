ee5867292262d2b6e0d360343c32b00d6831c36b
Brian Gann <bkgann@gmail.com>
2019-12-27 19:55:59 -0600

    prettier

--- src/customuid.test.ts
+++ src/customuid.test.ts
@@ -1,15 +1,15 @@
-import { CustomUID } from "./customuid";
+import { CustomUID } from './customuid';
 describe('customuuid test', () => {
   const text = 'random_urlpath';
-  let uid = new CustomUID(text);
+  const uid = new CustomUID(text);
 
   it('should generate an href', () => {
-    expect(uid.href).toBe("http://localhost/#O-random_urlpath-1");
+    expect(uid.href).toBe('http://localhost/#O-random_urlpath-1');
   });
   it('should generate an id', () => {
-    expect(uid.id).toBe("O-random_urlpath-1");
+    expect(uid.id).toBe('O-random_urlpath-1');
   });
   it('should generate an url', () => {
-    expect(uid.toString()).toBe("url(http://localhost/#O-random_urlpath-1)");
+    expect(uid.toString()).toBe('url(http://localhost/#O-random_urlpath-1)');
   });
 });
